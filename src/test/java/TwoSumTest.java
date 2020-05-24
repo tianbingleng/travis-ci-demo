b8f0c15df93c37321b7085076a27af458c9a3ddb
Tianbing Leng <tianbingleng@gmail.com>
2020-05-23 18:33:45 -0700

    update gitignore and travis file

--- src/test/java/TwoSumTest.java
+++ src/test/java/TwoSumTest.java
@@ -21,4 +21,13 @@ public class TwoSumTest {
         Assert.assertArrayEquals(null, result);
     }
 
+    @Test
+    public void test_3() {
+        int[] nums = new int[]{2,7,11,15};
+        int target = 13;
+        int[] result = testDrive.solution(nums, target);
+        int[] expect = new int[]{0, 2};
+        Assert.assertArrayEquals(expect, result);
+    }
+
 }
